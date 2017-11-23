bde2edc81e8b0d61fce5e3b9e3b8621ef5bde529
U-AD-ENT\u426827 <u426827@DTCF78682AB0460.ent.wfb.bank.corp>
2017-11-22 18:36:14 -0800

    added custom voter

--- src/main/java/com/wellsfargo/oas/security/config/WebSecurityConfig.java
+++ src/main/java/com/wellsfargo/oas/security/config/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.wellsfargo.oas.security.config;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Collection;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -10,8 +11,11 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
+import org.springframework.security.access.AccessDecisionVoter;
+import org.springframework.security.access.ConfigAttribute;
+import org.springframework.security.access.vote.AffirmativeBased;
 import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.config.annotation.ObjectPostProcessor;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -27,98 +31,97 @@ import com.wellsfargo.oas.security.saml.SamlAuthFilter;
 @Configuration
 @EnableWebSecurity
 @EnableAutoConfiguration(exclude = {org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration.class })
-public class WebSecurityConfig {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-  @Configuration
-  @Order(1)
-  public static class AccessPhraseWebSecurityConfig extends WebSecurityConfigurerAdapter {
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    http.csrf().disable().authorizeRequests().antMatchers("/", "/home", "/error")
+        .permitAll().anyRequest().authenticated()
+        .withObjectPostProcessor(new ObjectPostProcessor<AffirmativeBased>() {
 
-      http.csrf()
-          .disable()
-          .authorizeRequests()
-          .antMatchers("/", "/home", "/error")
-          .permitAll()
-          .anyRequest()
-          .authenticated()
-          .and()
-          .formLogin()
-          .loginPage("/login")
-          .permitAll()
-          .and()
-          .addFilterBefore(authFilter(), UsernamePasswordAuthenticationFilter.class)
-          .addFilterBefore(getSamlAuthFilter(),
-              UsernamePasswordAuthenticationFilter.class).logout().permitAll();
-    }
+          @Override
+          public AffirmativeBased postProcess(AffirmativeBased affirmativeBased) {
 
-    /*
-     * private AccessDecisionManager accessDecisionManager() {
-     * 
-     * return new UnanimousBased(Arrays.asList(getCustomVoter()));
-     * 
-     * }
-     * 
-     * private AccessDecisionVoter<? extends Object> getCustomVoter() {
-     * 
-     * return new AccessDecisionVoter<Object>() {
-     * 
-     * @Override public boolean supports(ConfigAttribute attribute) {
-     * 
-     * // TODO Auto-generated method stub return true; }
-     * 
-     * @Override public boolean supports(Class<?> clazz) {
-     * 
-     * // TODO Auto-generated method stub return true; }
-     * 
-     * @Override public int vote(Authentication authentication, Object object,
-     * Collection<ConfigAttribute> attributes) {
-     * 
-     * return AccessDecisionVoter.ACCESS_GRANTED; } }; }
-     */
-    private SamlAuthFilter getSamlAuthFilter() {
+            affirmativeBased.getDecisionVoters().add(0, myAccessDecisionVoter2()); // add
+            // after
+            // WebExpressionVoter
+            return affirmativeBased;
+          }
+        }).and().formLogin().loginPage("/login").permitAll().and()
+        .addFilterBefore(authFilter(), UsernamePasswordAuthenticationFilter.class)
+        .addFilterBefore(getSamlAuthFilter(), UsernamePasswordAuthenticationFilter.class)
+        .logout().permitAll();
+  }
 
-      SamlAuthFilter samlAuthFilter = new SamlAuthFilter("/samlEntry");
-      samlAuthFilter.setAuthenticationFailureHandler(new AuthenticationFailureHandler() {
+  private SamlAuthFilter getSamlAuthFilter() {
 
-        @Override
-        public void onAuthenticationFailure(HttpServletRequest request,
-            HttpServletResponse response, AuthenticationException exception)
-            throws IOException, ServletException {
+    SamlAuthFilter samlAuthFilter = new SamlAuthFilter("/samlEntry");
+    samlAuthFilter.setAuthenticationFailureHandler(new AuthenticationFailureHandler() {
 
-          response.sendRedirect("/error");
-        }
-      });
-      return samlAuthFilter;
-    }
+      @Override
+      public void onAuthenticationFailure(HttpServletRequest request,
+          HttpServletResponse response, AuthenticationException exception)
+          throws IOException, ServletException {
 
-    @Bean
-    UsernamePasswordAuthenticationFilter authFilter() {
+        response.sendRedirect("/error");
+      }
+    });
+    return samlAuthFilter;
+  }
 
-      UsernamePasswordAuthenticationFilter usernamePasswordAuthenticationFilter = new UsernamePasswordAuthenticationFilter();
+  private AccessDecisionVoter<? extends Object> myAccessDecisionVoter2() {
 
-      usernamePasswordAuthenticationFilter
-          .setAuthenticationDetailsSource(new AccessPhraseAuthenticationDetailsSource());
+    return new AccessDecisionVoter() {
 
-      usernamePasswordAuthenticationFilter.setAuthenticationSuccessHandler((
-          HttpServletRequest request, HttpServletResponse response,
-          Authentication authentication) -> {
+      @Override
+      public boolean supports(ConfigAttribute attribute) {
 
-        if ("application/json".equals(request.getHeader("content-type"))) {
+        // TODO Auto-generated method stub
+        return true;
+      }
 
-          response.getWriter().write("This is working fine");
-        }
-        else {
-          response.sendRedirect("/hello");
-        }
+      @Override
+      public boolean supports(Class clazz) {
 
-      });
+        // TODO Auto-generated method stub
+        return true;
+      }
 
-      usernamePasswordAuthenticationFilter.setAuthenticationManager(new ProviderManager(
-          Arrays.asList(new AccessPhraseAuthProvider())));
+      @Override
+      public int vote(Authentication authentication, Object object, Collection attributes) {
 
-      return usernamePasswordAuthenticationFilter;
-    }
+        // TODO Auto-generated method stub
+        return AccessDecisionVoter.ACCESS_ABSTAIN;
+      }
+    };
+  }
+
+  @Bean
+  UsernamePasswordAuthenticationFilter authFilter() {
+
+    UsernamePasswordAuthenticationFilter usernamePasswordAuthenticationFilter = new UsernamePasswordAuthenticationFilter();
+
+    usernamePasswordAuthenticationFilter
+        .setAuthenticationDetailsSource(new AccessPhraseAuthenticationDetailsSource());
+
+    usernamePasswordAuthenticationFilter.setAuthenticationSuccessHandler((
+        HttpServletRequest request, HttpServletResponse response,
+        Authentication authentication) -> {
+
+      if ("application/json".equals(request.getHeader("content-type"))) {
+
+        response.getWriter().write("This is working fine");
+      }
+      else {
+        response.sendRedirect("/hello");
+      }
+
+    });
+
+    usernamePasswordAuthenticationFilter.setAuthenticationManager(new ProviderManager(
+        Arrays.asList(new AccessPhraseAuthProvider())));
+
+    return usernamePasswordAuthenticationFilter;
   }
 }
